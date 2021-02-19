1a8560621b523f3e06a9e4b140c24951647dd83d
TopGek99 <arowe890@gmail.com>
2021-02-19 13:32:33 +1030

    moved style into style tag and removed style.css

--- src/generateWebPage.js
+++ src/generateWebPage.js
@@ -71,7 +71,33 @@ function generateWebPage(employeeArray,teamName) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${teamName}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/style.css">
+    <style>
+        header {
+            background-color:rebeccapurple;
+            padding: 2%;
+        }
+        #title {
+            color: white;
+            text-align: center;
+        }
+        h1 {
+            font-size: 50px;
+        }
+        #cards {
+            width: 100%;
+        }
+        .employee {
+            margin: auto;
+        }
+        a {
+            text-decoration: none;
+            color: black;
+        }
+        .emp-row {
+            margin: auto;
+            margin-top: 3%;
+        }
+    </style>
 </head>
 <body>
     <header>
